15cc694cc4854e8ed528dc5fab9b8136766451c9
yueming-fan <yfan010@uottawa.ca>
2022-02-12 12:12:49 +0800

    fixed some mistakes

--- project.js
+++ project.js
@@ -1,15 +1,15 @@
 var Account = (function() {
 	var entity = {
-		"bud": 0.0,
-		"mea": 0.0,
-		"sho": 0.0,
-		"dec": 0.0,
-		"ent": 0.0,
-		"els": 0.0
+		"bud": 0,
+		"mea": 0,
+		"sho": 0,
+		"dec": 0,
+		"ent": 0,
+		"els": 0
 	};
 
 	entity.getBud = function() {
-		return this.bud;
+		return (this.bud-(this.mea+this.sho+this.dec+this.ent+this.els));
 	}
 	entity.getMea = function() {
 		return this.mea;
@@ -24,36 +24,129 @@ var Account = (function() {
 		return this.ent;
 	}
 	entity.getEls = function() {
-		return this.els
+		return this.els;
 	}
 	entity.getTotal = function() {
-		alert(0);
+		return (this.mea+this.sho+this.dec+this.ent+this.els);
 	}
 
 	entity.setBud = function(num) {
-		this.bud = num;
+		this.bud = this.bud+num;
 	}
 	entity.setMea = function(num) {
-		this.mea = num;
+		this.mea = this.mea+num;
 	}
 	entity.setSho = function(num) {
-		this.sho = num;
+		this.sho = this.sho+num;
 	}
 	entity.setDec = function(num) {
-		this.dec = num;
+		this.dec = this.dec+num;
 	}
 	entity.setEnt = function(num) {
-		this.ent = num;
+		this.ent = this.ent+num;
 	}
+	entity.setEls = function(num) {
+		this.els = this.els+num;
+	}
+
+	return entity;
 }());
 
 window.onload = function() {
 	var total = document.getElementById("total");
+	var meals = document.getElementById("meals");
+	var shopping = document.getElementById("shopping");
+	var decoration = document.getElementById("decoration");
+	var entertainment = document.getElementById("entertainment");
+	var el = document.getElementById("else");
+	var budget = document.getElementById("budget");
+
+	var mea = document.getElementById("mea");
+	var sho = document.getElementById("sho");
+	var dec = document.getElementById("dec");
+	var ent = document.getElementById("ent");
+	var els = document.getElementById("els");
+	var bud = document.getElementById("bud");
 
 	total.addEventListener(
 		"click",
 		function () {
-			Account.getTotal();
+			window.alert("Your total tonsumption is "+Account.getTotal());
 		}, 
 		false);
+
+	meals.addEventListener(
+		"click",
+		function () {
+			window.alert("Your meals consumption is "+ Account.getMea());
+		}, 
+		false);
+	shopping.addEventListener(
+		"click",
+		function () {
+			window.alert("Your shopping consumption is "+ Account.getSho());
+		}, 
+		false);
+	decoration.addEventListener(
+		"click",
+		function () {
+			window.alert("Your decoration consumption is "+ Account.getDec());
+		}, 
+		false);
+	entertainment.addEventListener(
+		"click",
+		function () {
+			window.alert("Your entertainment consumption is "+ Account.getEnt());
+		}, 
+		false);
+	el.addEventListener(
+		"click",
+		function () {
+			window.alert("Your other consumption is "+ Account.getEls());
+		}, 
+		false);
+	budget.addEventListener(
+		"click",
+		function () {
+			window.alert("Your left budget is " + Account.getBud());
+		},
+		false);
+
+	mea.addEventListener(
+		"click",
+		function () {
+			Account.setMea(parseInt(window.prompt("enter your consumption: ")));
+		}, 
+		false);	
+	sho.addEventListener(
+		"click",
+		function () {
+			Account.setSho(parseInt(window.prompt("enter your consumption: ")));
+		}, 
+		false);	
+	dec.addEventListener(
+		"click",
+		function () {
+			Account.setDec(parseInt(window.prompt("enter your consumption: ")));
+		}, 
+		false);	
+	ent.addEventListener(
+		"click",
+		function () {
+			Account.setEnt(parseInt(window.prompt("enter your consumption: ")));
+		}, 
+		false);	
+	els.addEventListener(
+		"click",
+		function () {
+			Account.setEls(parseInt(window.prompt("enter your consumption: ")));
+		}, 
+		false);	
+	bud.addEventListener(
+		"click",
+		function () {
+			Account.setBud(parseInt(window.prompt("enter your budget: ")));
+		}, 
+		false);	
+	
 }
